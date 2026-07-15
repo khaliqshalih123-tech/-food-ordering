9efaf685c1ebd123e34a16c73017527ea14721f5
kaliaq <khaliqshalih123@gmail.com>
2026-07-16 03:41:30 +0530

    fix mongodb uri to use env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/foodorder')
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("DB Connected!");
     } catch (error) {
         console.log("DB Connection Failed:", error.message);
